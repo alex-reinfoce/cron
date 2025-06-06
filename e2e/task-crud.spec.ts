ec592f4c6a412558a83102091cf4df21206685f6
alex-reinforce <alex.lichangnan@gmail.com>
2025-06-07 04:43:00 +0800

    Refactor task creation test in e2e suite to use a task object for improved readability and maintainability; update assertions to verify task details after execution.

--- e2e/task-crud.spec.ts
+++ e2e/task-crud.spec.ts
@@ -3,12 +3,17 @@ import { test } from './utils';
 test.use({ viewport: { width: 1920, height: 1080 } });
 
 test.describe('Task CRUD', () => {
-  test('should create a task and run it successfully', async ({ page }) => {
+  test('create a business task', async ({ page }) => {
+    const newTask = {
+      apiURL: 'http://localhost:5000',
+      name: 'Mock Server',
+    };
+
     await page.getByRole('button', { name: 'Create Task' }).click();
     await page.getByRole('textbox', { name: 'Task Name' }).click();
-    await page.getByRole('textbox', { name: 'Task Name' }).fill('Mock Server');
+    await page.getByRole('textbox', { name: 'Task Name' }).fill(newTask.name);
     await page.getByRole('textbox', { name: 'Request URL' }).click();
-    await page.getByRole('textbox', { name: 'Request URL' }).fill('http://localhost:5000');
+    await page.getByRole('textbox', { name: 'Request URL' }).fill(newTask.apiURL);
     await page.getByRole('button', { name: 'Create Task' }).click();
     await page.getByRole('combobox', { name: 'Execution Frequency' }).click();
     await page.locator('.data-\\[state\\=open\\]\\:animate-in').first().click();
@@ -17,8 +22,16 @@ test.describe('Task CRUD', () => {
     await page.getByRole('combobox', { name: 'Execution Frequency' }).click();
     await page.getByRole('option', { name: 'Every Second' }).click();
     await page.getByRole('button', { name: 'Create Task' }).click();
-    await page.getByRole('row', { name: 'Mock Server Business Active' }).getByRole('button').click();
+    await page
+      .getByRole('row', { name: `${newTask.name} Business Active` })
+      .getByRole('button')
+      .click();
     await page.getByRole('menuitem', { name: 'Test Execute' }).click();
-    await page.getByText('Task executed successfully').click();
+    await page.getByText('Task executed successfully').isVisible();
+    await page.locator('[data-slot="badge"]').first().getByText('Active').isVisible();
+    await page.locator('[data-slot="badge"]').first().getByText('Business').isVisible();
+    await page.getByText(newTask.apiURL).isVisible();
+    await page.getByText('Every second').isVisible();
+    await page.getByText(newTask.name).isVisible();
   });
 });
